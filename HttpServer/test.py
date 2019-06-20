a31bab753d46b0d3bb235e272c246833f8e293b6
Spma <1240201962@qq.com>
2019-06-20 15:01:37 +0800

    httpserver 初步完成

---
+++ HttpServer/test.py
@@ -0,0 +1,18 @@
+"""
+用于httpserver的检测
+"""
+from socket import *
+import json
+
+s = socket()
+s.bind(("0.0.0.0", 8080))
+s.listen(5)
+c, addr = s.accept()
+
+data = c.recv(1024).decode()
+print(data)
+d = {"status": '200', 'data': "From test"}
+data = json.dumps(d)
+c.send(data.encode())
+c.close()
+s.close()
---
+++ WebFrame/webframe.py
@@ -0,0 +1,8 @@
+"""
+webframe.py
+
+功能:用于模拟网站后端应用工作流程
+"""
+from socket import *
+import json
+from settings import *
